854f2024d18b58c20853138490d2f7f499b28edd
William Bojczuk <williambojczuk@gmail.com>
2024-05-26 23:07:31 -0400

    updated

--- src/app/(mainsite)/components/homepage/Header/FreeEstimateForm/FreeEstimateForm.tsx
+++ src/app/(mainsite)/components/homepage/Header/FreeEstimateForm/FreeEstimateForm.tsx
@@ -14,34 +14,34 @@ export default function FreeEstimateForm() {
   return (
     <>
     <MessageStatus status={status} setStatus={setStatus}/>
-    <form ref={formRef} id="estimateForm" onSubmit={(evt)=>{sendEmail(evt, setStatus, {
+    <form ref={formRef} id="estimateForm1" onSubmit={(evt)=>{sendEmail(evt, setStatus, {
         receiverEmail: process.env.NEXT_PUBLIC_DELIVERY_EMAIL!,
         data: Object.fromEntries(new FormData(formRef.current))
     }, formRef.current)}}>
 
-        <h3 id="estimateFormTitle">Ask About Our Packages</h3>
+        <h3 id="estimateFormTitle1">Ask About Our Packages</h3>
 
         {/* START FIELDS */}
             
        
-            <div className="input-wrapper">
-                <input required type="text" name="Name" id="name_input" placeholder={"Name"} />
+            <div className="input-wrapper1">
+                <input required type="text" name="Name" id="name_input1" placeholder={"Name"} />
             </div>
             
-            <div className="input-wrapper">
-                <input required type="tel" name="Phone_Number" id="phonenum" placeholder={"Phone Number"} />
+            <div className="input-wrapper1">
+                <input required type="tel" name="Phone_Number" id="phonenum1" placeholder={"Phone Number"} />
             </div>
-            <div className="input-wrapper">
+            <div className="input-wrapper1">
                
-            <input required type="email" name="Email" id="email" placeholder={"Email"} />
+            <input required type="email" name="Email" id="email1" placeholder={"Email"} />
             </div>
 
-            <div className="input-wrapper">
-                <textarea required maxLength={5000} name="Details" id="messageInput" placeholder={"Additional details..."}></textarea>
+            <div className="input-wrapper1">
+                <textarea required maxLength={5000} name="Details" id="messageInput1" placeholder={"Additional details..."}></textarea>
             </div>
             
             <div className="center">
-                <a onClick={(evt)=>{evt.preventDefault(); formRef.current.requestSubmit();}} className={`main-link`} id="estimateFormSubmit" type="submit">Submit</a>
+                <a onClick={(evt)=>{evt.preventDefault(); formRef.current.requestSubmit();}} className={`main-link`} id="estimateFormSubmit1" type="submit">Submit</a>
             </div>
 
             {/* END FIELDS */}
