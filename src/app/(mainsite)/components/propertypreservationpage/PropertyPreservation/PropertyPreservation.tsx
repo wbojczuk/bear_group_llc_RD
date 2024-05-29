a29ffe167f84e9792b5a8238ab61c627e6a84bb9
William Bojczuk <williambojczuk@gmail.com>
2024-05-28 21:10:00 -0400

    uopdeate

--- src/app/(mainsite)/components/propertypreservationpage/PropertyPreservation/PropertyPreservation.tsx
+++ src/app/(mainsite)/components/propertypreservationpage/PropertyPreservation/PropertyPreservation.tsx
@@ -116,7 +116,7 @@ Our team uses high-quality locks and the latest techniques to ensure your new lo
 
         <div className={styles.itemWrapper}>
           <div className={styles.imgWrapper}>
-          <img className={styles.img} src="/img/services/lawn.webp" width={360} height={600} aria-hidden />
+          <img className={styles.img} src="/img/services/lawncare.webp" width={360} height={600} aria-hidden />
           </div>
 
           <div className={styles.content}>
