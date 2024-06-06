66c68dea1a44a06ccfc949a678713cc6dc2eb023
William Bojczuk <williambojczuk@gmail.com>
2024-06-05 23:04:59 -0400

    updated

--- src/app/(mainsite)/data/serviceData.ts
+++ src/app/(mainsite)/data/serviceData.ts
@@ -1,6 +1,6 @@
 const serviceData: string[] = [
     "Property_Services",
-    "Cleaning_Services",
+    // "Cleaning_Services",
     // "Commercial_Properties",
     // "Emergency_Services",
     // "Property_Maintenance",
