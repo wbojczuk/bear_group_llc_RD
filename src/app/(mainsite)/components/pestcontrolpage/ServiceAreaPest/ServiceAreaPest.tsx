51b2235d1051f8ed600fddd8833128796e048ad1
William Bojczuk <williambojczuk@gmail.com>
2025-01-06 07:48:24 -0500

    k

--- src/app/(mainsite)/components/pestcontrolpage/ServiceAreaPest/ServiceAreaPest.tsx
+++ src/app/(mainsite)/components/pestcontrolpage/ServiceAreaPest/ServiceAreaPest.tsx
@@ -4,16 +4,10 @@ import styles from './servicearea.module.css';
 export default function ServiceArea(){
 return (
  <div className={styles.serviceArea}>
-    <div className={styles.textWrapper}>
-        <h2>
-            <span className="underline">Serving All of</span> <br />
-            <span className="underline">Rode Island</span> <br />
-            <span className="underline">and Connecticut</span>
-        </h2>
-        <Link className='main-link' href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUM}`}>Call Now</Link>
-    </div>
+        
     <div className={styles.mapWrapper}>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d764323.3469830768!2d-72.15730448067099!3d41.555433046453665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e43514620ed70f%3A0x1e4e18bce7c106e7!2sRhode%20Island!5e0!3m2!1sen!2sus!4v1735608164137!5m2!1sen!2sus" style={{"border":0}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1mPUG0T9HA53homIvqFtN5x295NQ6p4M&ehbc=2E312F&noprof=1" style={{"border":0}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </div>
+    <Link className='main-link main-link-light' href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUM}`}>Call Now</Link>
  </div>
 )};
